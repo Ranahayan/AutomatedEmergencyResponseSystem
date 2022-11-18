2c0f59ecfef1ee0ef57161ce6edba5bbb10bdd39
Naeem-pro <2019cs232@student.uet.edu.pk>
2022-11-18 18:08:52 +0500

    Splach screen done

--- Screens/splash.js
+++ Screens/splash.js
@@ -1,12 +1,65 @@
-import { View, Text } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 
 const Splash = () => {
+  const handleTextClicked = () => {
+    console.log("Let's get start button clicked");
+  };
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Welcome to
+        <Text style={{ color: "#0165FF" }}>
+          {" "}
+          {"\n"}Automated {"\n"} Emergency Response {"\n"}System
+        </Text>
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          paddingLeft: 50,
+          paddingRight: 50,
+          marginTop: 100,
+        }}
+      >
+        In publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content.
+      </Text>
+      <TouchableOpacity onPress={handleTextClicked} style={styles.button}>
+        <Text style={styles.buttonText}>Let's get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Splash;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  title: {
+    color: "#474747",
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+    marginTop: 70,
+    lineHeight: 35,
+  },
+  button: {
+    backgroundColor: "#0165FF",
+    padding: 10,
+    borderRadius: 20,
+    width: 300,
+    marginTop: 120,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
